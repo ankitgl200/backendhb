9738d21282b1c51841dd32c785f349fc9e4a5b75
ankitgl200 <ukankitpro@gmail.com>
2026-05-04 00:27:12 +0530

    Update feedbacks.js

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -65,4 +65,35 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+
+router.get("/display", async (req, res) => {
+  try {
+    const data = await Feedback.find({
+      type: "review",
+      display: true
+    }).sort({ createdAt: -1 });
+
+    res.json({ success: true, data });
+  } catch (err) {
+    res.json({ success: false });
+  }
+});
+
+router.post("/toggle-display", async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    const item = await Feedback.findById(id);
+    item.display = !item.display;
+
+    await item.save();
+
+    res.json({ success: true, display: item.display });
+
+  } catch (err) {
+    res.json({ success: false });
+  }
+});
+
+
 module.exports = router;
