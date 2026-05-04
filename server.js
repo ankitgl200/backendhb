ade84de6bf374ca6c131d5d5e2de9848c033f7ef
ankitgl200 <ukankitpro@gmail.com>
2026-05-04 13:11:38 +0530

    Enhance server with cookie parser and admin routes
    Added cookie parser middleware and admin-only access control for specific routes.

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 // DB connect
 connectDB();
+function adminOnly(req, res, next) {
+  if (req.cookies.adminAuth === "true") return next();
+  res.status(401).json({ message: "Unauthorized" });
+}
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/products", require("./routes/products"));
-app.use("/api/orders", require("./routes/orders"));
-app.use("/api/feedback", require("./routes/feedbacks"));
+app.use("/api/products", adminOnly, require("./routes/products"));
+app.use("/api/orders", adminOnly, require("./routes/orders"));
+app.use("/api/feedback", adminOnly, require("./routes/feedbacks"));
 
 app.get("/", (req, res) => {
   res.send("Hostel Bites Backend Running 🚀");
