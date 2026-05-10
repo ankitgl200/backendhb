d6deb2da8474fc6319e4f20408bd88ae2f91fd6d
ankitgl200 <ukankitpro@gmail.com>
2026-05-11 01:59:11 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  phone: {
-    type: String,
-    unique: true
-  },
-  room: String,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+    name: String,
+    phone: {
+        type: String,
+        unique: true,
+        match: /^[6-9]\d{9}$/
+    },
+    room: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model("User", userSchema);
