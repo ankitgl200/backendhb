dbd3fc181b1889cf28e28d6f16f8487f69107516
ankitgl200 <ukankitpro@gmail.com>
2026-05-18 20:50:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(express.json());
@@ -26,6 +28,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api/orders", require("./routes/orders"));
 app.use("/api/feedback", require("./routes/feedbacks"));
+app.use("/api/overview", require("./routes/overview"));
 
 app.get("/", (req, res) => {
   res.send("Hostel Bites Backend Running 🚀");
