3f7554d6f56365337e23fc2350b5b9dd687148b5
ankitgl200 <ukankitpro@gmail.com>
2026-05-20 14:36:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ connectDB();
 
 // Routes
 app.use("/api/auths", require("./routes/auths"));
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api/orders", require("./routes/orders"));
 app.use("/api/feedback", require("./routes/feedbacks"));
